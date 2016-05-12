3578fb7e2967f1656e8103418aa529ddf7243ece
mickmister <mjkochell@gmail.com>
2016-05-12 13:21:13 -0400

    clean up audio analysis script

--- run.py
+++ run.py
@@ -1,45 +1,45 @@
-# Feature extraction example
 import numpy as np
 import librosa
 import csv
 
-print('1')
-# Load the example clip
-# filename = librosa.util.example_audio_file()
 filename = '6.wav'
+
+steps = 0
+def tick():
+  steps += 1
+  print(steps)
+
+tick()
+
+# filename = librosa.util.example_audio_file()
+
 y, sr = librosa.load(filename)
-print('2')
-# Separate harmonics and percussives into two waveforms
+tick()
+
+# separate harmonic and percussive
 y_harmonic, y_percussive = librosa.effects.hpss(y)
-print('3')
-# Beat track on the percussive signal
-tempo, beat_frames = librosa.beat.beat_track(y=y_percussive,
-                                             sr=sr)
-print('4')
-# Compute MFCC features from the raw signal
+tick()
+
+# analyze rhythm
+tempo, beat_frames = librosa.beat.beat_track(y=y_percussive, sr=sr)
+tick()
+
 mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
-print('5')
-# And the first-order differences (delta features)
+tick()
 mfcc_delta = librosa.feature.delta(mfcc)
-print('6')
-# Stack and synchronize between beat events
-# This time, we'll use the mean value (default) instead of median
-beat_mfcc_delta = librosa.feature.sync(np.vstack([mfcc, mfcc_delta]),
-                                       beat_frames)
-print('7')
-# Compute chroma features from the harmonic signal
-chromagram = librosa.feature.chroma_cqt(y=y_harmonic,
-                                        sr=sr)
-print('8')
-# Aggregate chroma features between beat events
-# We'll use the median value of each feature between beat frames
-beat_chroma = librosa.feature.sync(chromagram,
-                                   beat_frames,
-                                   aggregate=np.median)
-print('9')
-# Finally, stack all beat-synchronous features together
+tick()
+beat_mfcc_delta = librosa.feature.sync(np.vstack([mfcc, mfcc_delta]), beat_frames)
+tick()
+
+# analyze frequencies for every time slice
+chromagram = librosa.feature.chroma_cqt(y=y_harmonic, sr=sr)
+tick()
+
+beat_chroma = librosa.feature.sync(chromagram, beat_frames, aggregate=np.median)
+tick()
+
 beat_features = np.vstack([beat_chroma, beat_mfcc_delta])
-print('10')
+tick()
 
 outfile = 'chromagram.csv'
 chroma2 = np.transpose(chromagram)
@@ -52,7 +52,6 @@ beatchroma2 = np.transpose(beat_chroma)
 with open(outfile, 'wb') as f:
     writer = csv.writer(f)
     writer.writerows(beatchroma2)
-
  
 outfile = 'beat_features.csv'
 beatfeatures2 = np.transpose(beat_features)
